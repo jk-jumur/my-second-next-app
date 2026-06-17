1c4a684e5681f343382aa1b1ee02d6d4ca0a3c4b
jk-jumur <developer.jumur@gmail.com>
2026-06-17 18:46:08 +0600

    loading data using client component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
+import Counter from "@/Components/Counter";
+import Header from "@/Components/Header";
 
 
 export default function Home() {
+   console.log('next js server')
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
        
+          <Header></Header>
+          <Counter></Counter>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.js file.
